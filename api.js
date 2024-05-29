bbf08be47a74c79eb26401c76b5ea0c2162f15a8
emincan tetik <emincantetik16@gmail.com>
2024-05-29 23:42:04 +0300

    some changes were made

--- api.js
+++ api.js
@@ -10,7 +10,7 @@ const port=4000;
     {
         id:1,
         title:"Web Development Bootcamp",
-        content:"The web development bootcamp from Angela YU, teaches web development more than 1million students. In the course there are a lot of topics. HTML,CSS,JS,NODE.JS,BOOTSTRAP,JQUERY,EJS,REACT,API,DATABASES and ect. I recommend this course those who is interested in software. You create an infrastructure. ",
+        content:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
         author:"Emincan TETİK",
         date:new Date(),
     },
